4d64aa77d6c6c34ec0d26fb4fef70bcddfa333bf
tyler <tylercharawell@gmail.com>
2021-06-03 19:03:27 -0700

    commit tree 18864cac2ab7dc3080b932e1f12da6fb051659f6

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
-import MovieReviews from './MovieReviews'
+//import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+//const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
+//const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+            //+ `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+export default class LatestMovieReviewsContainer extends Component {
+    render() {
+      return  <div></div>
+    }
+}
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,18 @@
-// Code MovieReviews Here
+import React, { Component } from 'react';
+
+
+
+export default class MovieReviews extends Component {
+    
+componentDidMount() {
+    const url = "https://api.nytimes.com/svc/movies/v2/reviews/all.json"
+    const response = fetch(url)
+    const data = response.json
+    console.log(data)
+}
+    
+    
+    render() {
+        return <div></div>
+    }
+}
